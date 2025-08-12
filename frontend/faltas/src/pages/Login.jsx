669236ed1db068d0202f753fc8911208cadd416f
IgorHen0 <igorhenrique0472@gmail.com>
2025-08-12 18:41:39 -0300

    atualização páginas de login e cadastro

--- frontend/faltas/src/pages/Login.jsx
+++ frontend/faltas/src/pages/Login.jsx
@@ -1,46 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
 import './Auth.css';
 
 function Login({ onNavigate }) {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    }
 
     const handleNavigate = (e) => {
         e.preventDefault();
         onNavigate('signup');
     }
 
-  return (
-    <div className="auth-container">
-       <div className="auth-form">
-        <p className="auth-header-link">Login</p>
-        <h2 className="auth-title">Bem-vindo de volta!</h2>
-        <p className="auth-subtitle">Digite suas credenciais para acessar sua conta</p>
-        <form>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Digite seu email" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Senha</label>
-            <input type="password" id="password" placeholder="******" />
-            <a href="#" className="forgot-password">esqueceu a senha?</a>
-          </div>
-          <button type="submit" className="auth-button">Login</button>
-        </form>
-        {/* <div className="social-login">
+    return (
+        <div className="auth-container">
+            <div className="auth-form">
+                <p className="auth-header-link">Login</p>
+                <h2 className="auth-title">Bem-vindo de volta!</h2>
+                <p className="auth-subtitle">Digite suas credenciais para acessar sua conta</p>
+                <form>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" placeholder="Digite seu email" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Senha</label>
+                        <div className="password-group">
+                            <input type={showPassword ? "text" : "password"} id="password" placeholder="******" />
+                            <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
+                                {showPassword ? "🙈" : "👁️"}
+                            </span>
+                        </div>
+                        <a href="#" className="forgot-password">esqueceu a senha?</a>
+                    </div>
+                    <button type="submit" className="auth-button">Login</button>
+                </form>
+                {/* <div className="social-login">
           <button className="social-button google">
             <img src="https://img.icons8.com/color/16/000000/google-logo.png" alt="Google" />
             Sign in with Google
           </button>
         </div> */}
-        <p className="auth-footer">
-          Não tem uma conta? <a href="#" onClick={handleNavigate}>Cadastrar</a>
-        </p>
-      </div>
-      <div className="auth-image">
-        <img src="/ufmg.jpg" alt="Decorative Plant" />
-      </div>
-    </div>
-  );
+                <p className="auth-footer">
+                    Não tem uma conta? <a href="#" onClick={handleNavigate}>Cadastrar</a>
+                </p>
+            </div>
+            <div className="auth-image">
+                <img src="/ufmg.jpg" alt="Decorative Plant" />
+            </div>
+        </div>
+    );
 }
 
 export default Login;
